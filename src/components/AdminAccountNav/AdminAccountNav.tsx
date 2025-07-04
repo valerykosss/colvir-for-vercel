2ace91e48be97a62739090340e59193d793ef67a
valerykosss <valery,kosss@gmail.com>
2025-07-04 18:38:49 +0300

    nav and link styles added

--- src/components/AdminAccountNav/AdminAccountNav.tsx
+++ src/components/AdminAccountNav/AdminAccountNav.tsx
@@ -1,13 +1,14 @@
 'use client'
 import { signOut } from "next-auth/react";
 import Link from "next/link";
+import styles from './AdminAccountNav.module.css';
 
 export default function AdminAccountNav() {
     // console.log("logout btn");
     
    return (
-    <nav>
-        <Link href="/admin-dashboard">
+    <nav className={styles.nav}>
+        <Link className={styles.link} href="/admin-dashboard">
             Admin Panel
         </Link>
 
