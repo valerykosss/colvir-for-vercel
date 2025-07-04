6e2597e414833b55837db96fa49358b31630ef92
valerykosss <valery,kosss@gmail.com>
2025-07-04 17:55:22 +0300

    login form added redirect

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,28 @@
+// import { LoginForm } from "@/components/LoginForm/LoginForm";
+
+// export default function LoginPage() {
+//   return (
+//     <div>
+//       <LoginForm />
+//     </div>
+//   )
+// }
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
 import { LoginForm } from "@/components/LoginForm/LoginForm";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await getServerSession(authOptions);
+
+  if (session?.user) {
+    redirect("/admin-dashboard");
+  }
+
   return (
     <div>
       <LoginForm />
     </div>
-  )
+  );
 }
